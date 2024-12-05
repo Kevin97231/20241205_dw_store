324bcae19246127896b1e06beb46da169d18333f
klabarre <klabarre@dawan.fr>
2024-12-05 14:08:26 +0100

    AJout des skeletons sur les produits

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,12 +1,15 @@
 /* eslint-disable react/prop-types */
 import { Card } from "./Card";
+import { SkeletonCard } from "./SkeletonCard";
 
 export const ProductList = ({ products }) => {
+  const array = Array.from({ length: 8 });
+
   return (
     <div className="grid w-full grid-cols-4 gap-4">
-      {products.map((product) => (
-        <Card key={product.id} product={product} />
-      ))}
+      {products !== undefined && products.length > 0
+        ? products.map((product) => <Card key={product.id} product={product} />)
+        : array.map((_, index) => <SkeletonCard key={index} />)}
     </div>
   );
 };
