5861471e1e62e17bf6177ecd2899039196f00a59
klabarre <klabarre@dawan.fr>
2024-12-05 13:47:58 +0100

    ajout exmple redirection avec navigate

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,13 @@
+import { Link, useNavigate } from "react-router-dom";
+
 /* eslint-disable react/prop-types */
 export const Card = ({ product }) => {
+  // Exemple pour rediriger dans une fonction javascript
+  const navigate = useNavigate();
+  const navigateToDetail = () => {
+    navigate(`/products/${product.id}`);
+  };
+
   return (
     <div className="flex shadow-xl card bg-base-100 w-96">
       <figure>
@@ -12,7 +20,12 @@ export const Card = ({ product }) => {
         <p>{product.category}</p>
         <p>{product.decription}</p>
         <p>{product.price}</p>
-        <div className="justify-end card-actions"></div>
+        <div className="justify-end card-actions">
+          <button className="btn btn-primary" onClick={navigateToDetail}>
+            Voir
+          </button>
+          <Link to={`/products/${product.id}`}>Voir avec le Link</Link>
+        </div>
       </div>
     </div>
   );
