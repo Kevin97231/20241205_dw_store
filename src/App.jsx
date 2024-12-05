658a0d17b0194b9881488cedd5ccad836bb21cb2
klabarre <klabarre@dawan.fr>
2024-12-05 09:26:55 +0100

    first push

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import "./App.css";
+import { Navbar } from "./components/Navbar";
 
 function App() {
-  return <></>;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root />,
+      children: [
+        { path: "", element: <>Page d'accueil</> },
+        { path: "/products", element: <>page products</> },
+        { path: "*", element: <>404 NOT FOUND</> },
+      ],
+    },
+  ]);
+
+  function Root() {
+    return (
+      // <Navbar />
+      <>
+        <Navbar />
+        <Outlet />
+      </>
+    );
+  }
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+import { HeaderItem } from "./HeaderItem";
+
+export const Navbar = () => {
+  const logoPath = "/src/assets/images/logo.png";
+
+  return (
+    <header className="navbar px-5 shadow-lg navbar bg-neutral txt-neutral-content">
+      <nav className="w-full">
+        <Link to={"/"}>
+          <img src={logoPath} alt="logo dw_store" className="h-10" />
+        </Link>
+
+        <div className="m-auto w-fit">
+          <ul className="flex items-center gap-10">
+            <li>
+              <HeaderItem href={"/products"}>Nos produits</HeaderItem>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
